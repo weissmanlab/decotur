9966e06f9b03855a616aa92abcacff85119f1ea7
rohansmehta <46578445+rohansmehta@users.noreply.github.com>
2026-09-18 16:48:04 -0500

    Document .verbose_message function parameters
    Add documentation for .verbose_message function

--- R/verbose_message.R
+++ R/verbose_message.R
@@ -1,4 +1,10 @@
+#' .expand_pattern_scores
+#'
+#' This helper function takes compressed pattern scores
+#' and restores the human-readable gene name table.
 
+#'@param verbose Boolean from user input
+#'@param ... Message to print
 
 
 .verbose_message <- function(verbose, ...) {
